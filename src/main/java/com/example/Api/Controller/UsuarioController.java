b67e4494a804d640d1d672d4aa0bf4f5371639e4
david <escadonblaster41@gmail.com>
2026-02-16 22:27:39 -0500

    Paquetes

--- src/main/java/com/example/Api/Controller/UsuarioController.java
+++ src/main/java/com/example/Api/Controller/UsuarioController.java
@@ -1,7 +1,37 @@
 package com.example.Api.Controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
+@RequestMapping("api")
 public class UsuarioController {
+
+    @GetMapping("Crear")
+    public void CrearUsuario (){
+
+
+    }
+
+    @GetMapping("List")
+    public void Verlist (){
+
+
+    }
+
+    @PostMapping("Params")
+    public void Paramas (){
+
+
+    }
+
+
+
+
+
+
+
 }
--- src/main/java/com/example/Api/DTo/UsuarioDTO.java
+++ src/main/java/com/example/Api/DTo/UsuarioDTO.java
@@ -1,4 +1,33 @@
 package com.example.Api.DTo;
 
 public class UsuarioDTO {
+
+
+    String  nombre;
+    String password;
+
+    public UsuarioDTO() {
+
+    }
+
+    public UsuarioDTO(String nombre, String password) {
+        this.nombre = nombre;
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
